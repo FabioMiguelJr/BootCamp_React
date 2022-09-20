3c0f4a3ee526d67b400d8381b83cbb3a72445757
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-09-19 22:33:53 -0300

    Modularização em javascript

---
+++ Modulo_00/Modularizacao/modulo1.js
@@ -0,0 +1,13 @@
+function soma(a, b) {
+    return a + b;
+}
+
+export function subtrai(a, b) {
+    return a - b;
+}
+
+export const PI = 3.14;
+
+export { soma };
+
+console.log("Carregou");
---
+++ Modulo_00/Modularizacao/modulo2.js
@@ -0,0 +1,4 @@
+import { PI, soma as adiciona, subtrai } from "./modulo1.js";
+
+console.log(adiciona(PI, 2));
+console.log(subtrai(2, 2));
