6c618b1bacdc8d0289dd1c247b5490b93781232d
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-10-01 13:39:57 -0300

    Funções imediatas - IIFE

---
+++ Modulo_01/08_Javascript_avancado_2/IIFE_Funcoes_Imediatas/index0.js
@@ -0,0 +1,37 @@
+// IIFE
+
+// Funcion Declaration
+// function myFunction() {
+//     - código -
+// }
+
+// Funcion Expression
+// let myFunction = function() {
+//     - código -
+// }
+
+// IIFE - Sintaxe básica
+
+// (function () {} ) ();
+
+// EXEMPLO DE IIFE PARA DECLARAÇÃO DE FUNÇÃO
+
+(function mensagem() {
+    console.log("Exemplo de IIFE");
+})();
+
+(function () {
+    console.log("Exemplo 2 de IIFE");
+})();
+
+const unicoID = (function () {
+    let count = 0;
+    return function () {
+        ++count;
+        return `id_${count}`;
+    };
+})();
+
+console.log(unicoID());
+console.log(unicoID());
+console.log(unicoID.count);
