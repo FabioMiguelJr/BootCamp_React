a1ae1e0ccaf5b04cb5e3b40da33eed779c0ffb7d
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-10-03 16:21:08 -0300

    Iterator ajustes

--- Modulo_01/07_Javascript_avancado_1/07.6_Javascript_Assincrono/07.6.3_Iterators/index0.js
+++ Modulo_01/07_Javascript_avancado_1/07.6_Javascript_Assincrono/07.6.3_Iterators/index0.js
@@ -43,7 +43,7 @@ const carModelAll = {
                     currentModelIndex = 0;
                 }
                 // Verifica se já navegou em todas as marcas
-                if (!(currentBrandIndex < models.length)) {
+                if (!(currentBrandIndex < brands.length)) {
                     return {
                         value: undefined,
                         done: true,
