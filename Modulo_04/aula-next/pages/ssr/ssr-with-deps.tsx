97d524700f906f0809a922efeb8c12ad71e86f81
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-11-11 16:59:45 -0300

    Aula - Mód 04 - Pre-rendering - SSR Ajustes

--- Modulo_04/aula-next/pages/ssr/ssr-with-deps.tsx
+++ Modulo_04/aula-next/pages/ssr/ssr-with-deps.tsx
@@ -3,15 +3,17 @@ export async function getServerSideProps() {
 		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=10&page=1&sparkline=false"
 	).then((res) => res.json());
 
-	return { props: coins };
+	return { props: { coins, propTest: "This is a test" } };
 }
 
-const SsrWithDeps = (coins: any[]) => {
+const SsrWithDeps = ({ coins, propTest }: { coins: any[]; propTest: string }) => {
 	return (
 		<ul>
 			{coins.map((item: any) => (
 				<li key={item.id}>
 					<a>{item.name}</a>
+					<br />
+					<a>{item.symbol}</a>
 				</li>
 			))}
 		</ul>
