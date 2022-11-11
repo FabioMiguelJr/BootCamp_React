91300eacb1200034991fc4971be5c30affd2133c
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-11-11 17:25:49 -0300

    Aula - Mód 04 - Pre-rendering - ISR

---
+++ Modulo_04/aula-next/pages/isr/index.tsx
@@ -0,0 +1,28 @@
+export async function getStaticProps() {
+	const coins = await fetch(
+		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=10&page=1&sparkline=false"
+	).then((res) => res.json());
+
+	const date = new Date();
+	// return { props: { coins, propTest: "This is a test" } };
+	return { props: { coins, lastRenderer: date.getSeconds() }, revalidate: 5 };
+}
+
+const Isr = (props: any) => {
+	const coins = props.coins;
+
+	return (
+		<div>
+			<div>{props.lastRenderer}</div>
+			<ul>
+				{coins.map((item: any) => (
+					<li key={item.id}>
+						<a>{item.name}</a>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default Isr;
