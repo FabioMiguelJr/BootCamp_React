33a0f13a70ba52e0fc56ece8d7bcaab92662f804
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-11-14 17:24:52 -0300

    Acessando fakestoreapi.com

--- Modulo_04/aula-next/pages/isr/index.tsx
+++ Modulo_04/aula-next/pages/isr/index.tsx
@@ -1,26 +1,24 @@
 import Link from "next/link";
 
 export async function getStaticProps() {
-	const coins = await fetch(
-		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=10&page=1&sparkline=false"
-	).then((res) => res.json());
+	const products = await fetch("https://fakestoreapi.com/products").then((res) => res.json());
 
 	const date = new Date();
 	// return { props: { coins, propTest: "This is a test" } };
-	return { props: { coins, lastRenderer: date.getSeconds() }, revalidate: 5 };
+	return { props: { products, lastRenderer: date.getSeconds() } };
 }
 
-const Isr = (props: any) => {
-	const coins = props.coins;
+const Isr = (props?: any) => {
+	const products = props.products;
 
 	return (
 		<div>
 			<div>{props.lastRenderer}</div>
 			<ul>
-				{coins.map((item: any) => (
+				{products.map((item: any) => (
 					<li key={item.id}>
 						<Link href={`isr/${item.id}`} passHref legacyBehavior>
-							<a>{item.name}</a>
+							<a>{item.title}</a>
 						</Link>
 					</li>
 				))}
