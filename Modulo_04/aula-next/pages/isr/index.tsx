add0bdc8ac6f6cf4af395ba49fe37fe2f7c1fc07
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-11-14 16:28:41 -0300

    Aula - Mód. 04 - Rotas p/ as páginas criadas

--- Modulo_04/aula-next/pages/isr/index.tsx
+++ Modulo_04/aula-next/pages/isr/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export async function getStaticProps() {
 	const coins = await fetch(
 		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=10&page=1&sparkline=false"
@@ -17,7 +19,9 @@ const Isr = (props: any) => {
 			<ul>
 				{coins.map((item: any) => (
 					<li key={item.id}>
-						<a>{item.name}</a>
+						<Link href={`isr/${item.id}`} passHref legacyBehavior>
+							<a>{item.name}</a>
+						</Link>
 					</li>
 				))}
 			</ul>
