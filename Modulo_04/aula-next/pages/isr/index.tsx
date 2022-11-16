b2fc456fa4003f788530c0e6a4f1ce895e56c268
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-11-16 10:03:34 -0300

    16/11/2022

--- Modulo_04/aula-next/pages/isr/index.tsx
+++ Modulo_04/aula-next/pages/isr/index.tsx
@@ -2,21 +2,33 @@ import Link from "next/link";
 import React from "react";
 
 export async function getStaticProps() {
-	const products = await fetch("https://fakestoreapi.com/products?limit=10").then((res) =>
-		res.json()
-	);
+	// const products = await fetch("https://fakestoreapi.com/products?limit=10").then((res) =>
+	// 	res.json()
+	// );
+	const options = {
+		method: "GET",
+		headers: {
+			"X-RapidAPI-Key": "0885f39f81mshb14270c99f3f7fcp178ec0jsn762ce601e3c5",
+			"X-RapidAPI-Host": "computer-components-api.p.rapidapi.com",
+		},
+	};
+	const products = await fetch(
+		"https://computer-components-api.p.rapidapi.com/case_fan?limit=10&offset=0",
+		options
+	).then((res) => res.json());
 
 	const date = new Date();
 	// return { props: { coins, propTest: "This is a test" } };
 	return { props: { products, lastRenderer: date.getSeconds() } };
 }
 
-const Isr = (props?: any) => {
+const Isr = (props: any) => {
 	const products = props.products;
 
 	return (
 		<>
-			<header>
+			{console.log(products)}
+			{/* <header>
 				<div className="bg-pink-200 mx-auto p-4">
 					<h1 className="text-center font-semibold text-2xl">Plataforma On-line</h1>
 				</div>
@@ -27,12 +39,12 @@ const Isr = (props?: any) => {
 					{products.map((item: any) => (
 						<li key={item.id}>
 							<Link href={`isr/${item.id}`} passHref legacyBehavior>
-								<a>{item.title}</a>
+								<p>{item}</p>
 							</Link>
 						</li>
 					))}
 				</ul>
-			</div>
+			</div> */}
 		</>
 	);
 };
