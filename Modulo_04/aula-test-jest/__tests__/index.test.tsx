b80dfce301637fed462b1f426299a8c4ff825c5b
DESKTOP-BM10C7V\Fabio <fabio.migueljr@gmail.com>
2022-11-17 14:38:44 -0300

    Aula - Mód.04 - TDD

--- Modulo_04/aula-test-jest/__tests__/index.test.tsx
+++ Modulo_04/aula-test-jest/__tests__/index.test.tsx
@@ -1,5 +1,7 @@
 import Home from "@/pages/index";
 
+// Praticando Jest
+
 const sum = (a: number, b: number) => {
 	return a + b;
 };
@@ -15,5 +17,38 @@ describe("Simple operations", () => {
 
 	test("dois mais dois é quatro", () => {
 		expect(sum(2, 2)).toBe(4);
+	});
+});
+
+// Praticando TDD
+
+const captalizeWord = (word: string) => {
+	const firstLetter = word[0].toUpperCase();
+	const otherLetters = word.substring(1);
+	return `${firstLetter}${otherLetters}`;
+};
+
+const captalize = (text: string) => {
+	if (text.length === 0) {
+		return "";
+	}
+
+	// const words = text.split(" ");
+	// return words.map((word) => captalizeWord(word)).join(" ");
+
+	return text.split(" ").map(captalizeWord).join(" ");
+};
+
+describe("Sanity of formater", () => {
+	test("Should do nothing for empty entry", () => {
+		expect(captalize("")).toBe("");
+	});
+
+	test("Should returno Rodrigo for Rodrigo", () => {
+		expect(captalize("rodrigo")).toBe("Rodrigo");
+	});
+
+	test("Should returno Rodrigo for Rodrigo", () => {
+		expect(captalize("rodrigo borba")).toBe("Rodrigo Borba");
 	});
 });
